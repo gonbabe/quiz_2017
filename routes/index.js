53a78880e1bd807f3cebaaaa9feedcd3506daf77
gonbabe <gbac95@gmail.com>
2017-06-04 13:24:35 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -26,6 +26,9 @@ router.delete('/quizzes/:quizId(\\d+)',    quizController.destroy);
 router.get('/quizzes/:quizId(\\d+)/play',  quizController.play);
 router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
 
+router.get('/quizzes/randomplay',  quizController.randomplay);
+router.get('/quizzes/randomcheck/:quizId(\\d+)?', quizController.randomcheck);
+
 // Pagina de Ayuda
 router.get('/help', function(req, res, next) {
     res.render('help');
